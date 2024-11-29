12720d1aabebcb9e2f25a65676186aba4567c276
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-29 19:36:41 -0400

    Actualizar handler.js

--- system/handler.js
+++ system/handler.js
@@ -403,16 +403,16 @@ export async function handler(chatUpdate) {
 
 global.dfail = (type, m, conn, usedPrefix) => {
     let msg = {
-        rowner: `✯ Hola, este comando solo puede ser utilizado por el *Creador* de la Bot.`,
-        owner: `✯ Hola, este comando solo puede ser utilizado por el *Creador* de la Bot y *Sub Bots*.`,
-        mods: `✯ Hola, este comando solo puede ser utilizado por los *Moderadores* de la Bot.`,
-        premium: `✯ Hola, este comando solo puede ser utilizado por Usuarios *Premium*.`,
-        group: `✯ Hola, este comando solo puede ser utilizado en *Grupos*.`,
-        private: `✯ Hola, este comando solo puede ser utilizado en mi Chat *Privado*.`,
-        admin: `✯ Hola, este comando solo puede ser utilizado por los *Administradores* del Grupo.`,
-        botAdmin: `✯ Hola, la bot debe ser *Administradora* para ejecutar este Comando.`,
-        unreg: `✯ Hola, para usar este comando debes estar *Registrado.*\n\nPara usar el bot debes registrarte primero\n\nUtiliza: */reg nombre.edad*\n\n_Ejemplo: */reg おDanịel.xyz⁩.666*_\n\nNo pongas los * *`,
-        restrict: `✯ Hola, esta característica está *deshabilitada.*`  
+        rowner: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MI CREADOR!!```", 
+        owner: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MI DESARROLLADOR!!```", 
+        mods: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA POR MIS DESARROLLADORES!!```", 
+        premium: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCIÓN SOLO ES PARA USUARIOS PREMIUM!!```", 
+        group: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCION SOLO PUEDE SER EJECUTADA EN GRUPOS!!```", 
+        private: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA FUNCIÓN SOLO PUEDE SER USADA EN CHAT PRIVADO!!```", 
+        admin: "```:⁖֟⊱┈֟፝❥ ¡¡ESTE COMANDO SOLO PUEDE SER USADO POR ADMINS!!```", 
+        botAdmin: "```:⁖֟⊱┈֟፝❥ ¡¡PARA USAR ESTA FUNCIÓN DEBO SER ADMIN DEL GRUPO!!```", 
+        unreg: "```:⁖֟⊱┈֟፝❥ ¡¡NECESITAS ESTAR REGISTRADO(A) PARA USAR ESTE COMANDO, ESCRIBE #reg PARA REGISTRARTE!!```",
+        restrict: "```:⁖֟⊱┈֟፝❥ ¡¡ESTA CARACTERÍSTICA ESTA DESACTIVADA!!```"
     }[type]
     if (msg) return conn.reply(m.chat, msg, m, rcanal).then(_ => m.react('✖️'))
 }
