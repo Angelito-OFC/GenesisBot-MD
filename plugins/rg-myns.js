1ac2e16ea0154bf97364490457b445b95d1335bb
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-29 20:07:13 -0400

    Actualizar rg-myns.js

--- plugins/rg-myns.js
+++ plugins/rg-myns.js
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 await conn.reply(m.chat, `${sn}`, m, rcanal)
 }
 handler.help = ['sn']
