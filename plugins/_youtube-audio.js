16f9d6f93214aa23ed753fefd5cbb844a9ac6c3d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-30 12:14:18 -0400

    Actualizar _youtube-audio.js

--- plugins/_youtube-audio.js
+++ plugins/_youtube-audio.js
@@ -56,3 +56,74 @@ handler.customPrefix = /^(Audio|audio)/;
 handler.command = new RegExp;
 
 export default handler; */
+
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
+    if (!m.quoted) {
+        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m)
+            .then(_ => m.react('✖️'));
+    }
+
+    if (!m.quoted.text.includes("*\`【Y O U T U B E - P L A Y】\`*")) {
+        return conn.reply(m.chat, `*\`Etiqueta el mensaje que contenga el resultado del Play.🤍\`*`, m)
+            .then(_ => m.react('✖️'));
+    }
+
+    let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'));
+
+    if (!urls) {
+        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m).then(_ => m.react('✖️'));
+    }
+
+    if (urls.length < parseInt(text)) {
+        return conn.reply(m.chat, `*\`Resultado no Encontrado.🤍\`*`, m).then(_ => m.react('✖️'));
+    }
+
+    let user = global.db.data.users[m.sender];
+
+    await m.react('🕓');
+    try {
+        let videoUrl = urls[0];
+        let response = await fetch(`https://api.siputzx.my.id/api/d/ytmp3?url=${videoUrl}`);
+        let result = await response.json();
+
+        if (!result.status) {
+            throw new Error('Error al obtener datos de la API');
+        }
+
+        let { title, dl: audio, author, thumb: thumbnail, duration } = result.data;
+
+        await conn.sendMessage(m.chat, { 
+            audio: { url: audio }, 
+            mimetype: "audio/mp4", 
+            fileName: `${title}.mp3`, 
+            quoted: m, 
+            contextInfo: {
+                'forwardingScore': 200,
+                'isForwarded': true,
+                externalAdReply: {
+                    showAdAttribution: false,
+                    title: title,
+                    body: author,
+                    mediaType: 2, 
+                    sourceUrl: videoUrl,
+                    thumbnail: await (await fetch(thumbnail)).buffer()
+                }
+            }
+        }, { quoted: m });
+
+        await m.react('✅');
+    } catch (e) {
+        console.error(e);
+        await conn.reply(m.chat, `*\`Hubo un error al procesar la descarga.🤍\`*`, m).then(_ => m.react('✖️'));
+    }
+};
+
+handler.help = ['Audio'];
+handler.tags = ['downloader'];
+handler.customPrefix = /^(Audio|audio)/;
+handler.command = new RegExp;
+
+export default handler;
+
