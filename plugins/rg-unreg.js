6fa987d47b871e68fda16713183b23351f87ae7c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-29 20:07:42 -0400

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -1,15 +1,14 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { conn, args, usedPrefix}) {
-  if (!args[0]) return m.reply(`🚩 Ingresa tu número de serie junto al comando.`)
+  if (!args[0]) return m.reply(`🤍 Ingresa tu número de serie junto al comando.`)
   let user = global.db.data.users[m.sender]
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) return m.reply('🚩 Número de serie incorrecto')
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
+  if (args[0] !== sn) return m.reply('🤍 Número de serie incorrecto')
   user.registered = false
-  m.reply(`🚩 Registro eliminado.`)
+  m.reply(`🤍 Registro eliminado.`)
 }
 handler.help = ['unreg'] 
 handler.tags = ['rg']
-
 handler.command = ['unreg'] 
 handler.register = true
 
