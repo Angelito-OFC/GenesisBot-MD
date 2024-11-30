85a7450db32ece8158cc259b0cab23f1038c4d98
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-29 20:05:59 -0400

    Actualizar rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -16,19 +16,21 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   user.age = age
   user.regTime = +new Date()
   user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
   let img = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/J5YVhwt/file.jpg')
   
   let now = new Date()
   let date = now.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' })
   let time = now.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit' })
-  
-  let txt = '*`—  R E G I S T R O  〤  U S E R`*\n\n'
-      txt += `\t\t*» Tag* :: @${m.sender.split('@')[0]}\n`
-      txt += `\t\t*» Nombre* :: ${name}\n`
-      txt += `\t\t*» Edad* :: ${age} años\n\n`
-      txt += `\t\t*» Fecha* :: ${date}\n`
-      txt += `\t\t*» Hora* :: ${time}\n\n`
+
+ let txt = '*`📄 R E G I S T R O 📄`*\n'
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
+      txt += `*\`⁘ TAG:\`* @${m.sender.split('@')[0]}\n`
+      txt += `*\`⁘ NOMBRE:\`* ${name}\n`
+      txt += `*\`⁘ EDAD:\`* ${age} años\n`
+      txt += `*\`⁘ FECHA:\`* ${date}\n`
+      txt += `*\`⁘ N° SERIAL:\`* ${sn}\n`
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
       txt += `> Escribe *${usedPrefix}profile* para ver tu perfil.`
       
   await conn.sendFile(m.chat, img, 'perfil.jpg', txt, m, false, { mentions: [m.sender] })
@@ -41,3 +43,46 @@ handler.tags = ['rg']
 handler.command = ['verify', 'reg', 'register', 'registrar']
 
 export default handler
+
+
+/* import { createHash } from 'crypto'
+import fs from 'fs'
+import fetch from 'node-fetch'
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { conn, text, usedPrefix, command }) {
+  let user = global.db.data.users[m.sender]
+  let name2 = conn.getName(m.sender)
+  if (user.registered === true) return m.reply(`🤍 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`🤍 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) return m.reply('🤍 El nombre no puede estar vacío.')
+  if (!age) return m.reply('🤍 La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🤍 El nombre es demasiado largo.' )
+  age = parseInt(age)
+  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+  let img = await (await fetch(`https://pomf2.lain.la/f/rycjgv2t.jpg`)).buffer()
+  let txt = '*`📄 R E G I S T R O 📄`*\n'
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
+      txt += `*\`⁘ NOMBRE:\`* ${name}\n`
+      txt += `*\`⁘ EDAD:\`* ${age} años\n`
+      txt += `*\`⁘ FECHA:\`* ${fecha}\n`
+      txt += `*\`⁘ N° SERIAL:\`* ${sn}\n`
+      txt += `\`━━━━━━━━━━━━━━━━━━━━\`\n`
+await conn.sendFile(m.chat, img, 'img.jpg', txt, m, null, fake)
+
+// await conn.sendFile(m.chat, botname, textbot, txt, img, img, canal, m)
+await m.react('✅')
+}
+handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
+handler.tags = ['start']
+
+handler.command = ['verify', 'reg', 'register', 'registrar'] 
+
+export default handler */
