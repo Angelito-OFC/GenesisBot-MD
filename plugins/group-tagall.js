02aab8d03f206c42678c780427a0f06eba9808d4
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-02 17:56:44 -0400

    Actualizar plugins

---
+++ plugins/group-tagall.js
@@ -0,0 +1,23 @@
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+m.react('🤍') 
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+const oi = `*» INFO :* ${pesan}`;
+  let teks = `*!  MENCION GENERAL  !*\n  *PARA ${participants.length} MIEMBROS* 🗣️\n\n ${oi}\n\n╭  ┄ 𝅄  ۪꒰ ׅ \`${packname}\` ׅ ꒱  ۟  𝅄 ┄\n`;
+  for (const mem of participants) {
+    teks += `┊🤍 @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `╰⸼ ┄ ┄ ┄ ─  ꒰  ׅ୭ *${vs}* ୧ ׅ ꒱  ┄  ─ ┄ ⸼`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['todos <mesaje>'];
+handler.tags = ['grupo'];
+handler.command = /^(tagall|invocar|marcar|todos|invocación)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
