a62dd40bf8fd9b90d7420472fd032616fefe1d40
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-03 19:13:57 -0400

    Actualizar _youtube-audio.js

--- plugins/_youtube-audio.js
+++ plugins/_youtube-audio.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) =
 
          await conn.sendMessage(m.chat, { image: { url: cxf.thumbnail }, caption: txt }, { quoted: m });
         await conn.sendMessage(m.chat, { audio: { url: cxf.dl_url }, fileName: `${cxf.title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
-        await m.react('🍷');
+        await m.react('✅');
     } catch (error2) {
         await m.react('✖️');
         return m.reply(`Ocurrió un error al procesar tu solicitud. Intenta nuevamente más tarde.`);
