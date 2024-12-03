50edcc4e837b270a7daf0e02c4f5c801297f6cd6
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-03 19:27:43 -0400

    Actualizar _youtube-video.js

--- plugins/_youtube-video.js
+++ plugins/_youtube-video.js
@@ -1,7 +1,7 @@
 import Starlights from '@StarlightsTeam/Scraper'
 let limit = 300
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
-if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
+if (!m.quoted) return // conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
 if (!m.quoted.text.includes("*\`【Y O U T U B E - P L A Y】\`*")) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m, rcanal).then(_ => m.react('✖️'))
 let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
 if (!urls) return conn.reply(m.chat, `Resultado no Encontrado.`, m, rcanal).then(_ => m.react('✖️'))
