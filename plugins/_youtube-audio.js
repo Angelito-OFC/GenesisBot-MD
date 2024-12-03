9b8abd7fdabcc54f4df83de7cd5510defa7f1393
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-03 19:20:57 -0400

    Actualizar _youtube-audio.js

--- plugins/_youtube-audio.js
+++ plugins/_youtube-audio.js
@@ -2,6 +2,43 @@ import fetch from 'node-fetch';
 import Sph from 'ytdl-mp3';
 
 let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
+    // Verificar si el mensaje citado existe y contiene el resultado de "YouTube Play"
+    if (!m.quoted || !m.quoted.text.includes("*`【Y O U T U B E - P L A Y】`*")) {
+        return m.react('✖️'); // Solo se reacciona con un error, sin enviar un mensaje
+    }
+
+    // Extraer URLs del mensaje citado
+    let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9_-]+)/, 'gi'));
+    if (!urls) return m.react('✖️'); // Si no se encuentra la URL, solo se reacciona con error
+    if (urls.length < text) return m.react('✖️'); // Si no se encuentran suficientes resultados, solo se reacciona con error
+
+    let user = global.db.data.users[m.sender];
+    let videoUrl = urls[0];
+
+    await m.react('🕓'); // Reacción de "cargando"
+
+    try {
+        let cxf = await Sph.ytdl(videoUrl);
+        await conn.sendMessage(m.chat, { audio: { url: cxf.dl_url }, fileName: `${cxf.title}.mp3`, mimetype: 'audio/mp4' }, { quoted: m });
+        await m.react('✅'); // Reacción de éxito
+    } catch (error2) {
+        await m.react('✖️'); // Reacción de error
+    }
+};
+
+handler.help = ['A'];
+handler.tags = ['downloader'];
+handler.customPrefix = /^(a|A)/; // Prefijo personalizado
+handler.command = new RegExp();
+
+export default handler;
+
+
+
+/*  import fetch from 'node-fetch';
+import Sph from 'ytdl-mp3';
+
+let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
     if (!m.quoted) return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m).then(() => m.react('✖️'));
     if (!m.quoted.text.includes("*`【Y O U T U B E - P L A Y】`*")) {
         return conn.reply(m.chat, `[ ✰ ] Etiqueta el mensaje que contenga el resultado de YouTube Play.`, m).then(() => m.react('✖️'));
@@ -38,4 +75,4 @@ handler.tags = ['downloader'];
 handler.customPrefix = /^(a|A)/; // Prefijo personalizado
 handler.command = new RegExp();
 
-export default handler;
+export default handler; */
