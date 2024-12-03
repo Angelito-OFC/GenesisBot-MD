d728f0f12a5159eb53bedd22d57bf9d4c1caf276
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-03 19:35:19 -0400

    Actualizar youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn: star, command, args, text, usedPrefix }) => {
        txt += `> *\`PUBLICADO:\`* ${eYear(res[0].ago)}\n`
        txt += `> *\`CANAL:\`* ${res[0].author.name || 'Desconocido'}\n`
        txt += `> *\`URL:\`* ${'https://youtu.be/' + res[0].videoId}\n\n`
-       txt += `> *-* _Etiqueta este mensaje con la opción a descargar_\n\n\`v\` (para descargar el video.)\n\`a\` (para descargar el audio.)`
+       txt += `> *-* _Etiqueta este mensaje con la opción a descargar_\n\nEtiqueta con \`v\` (para el video.)\nEtiqueta con \`a\` (para el audio.)`
 await star.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, fake)
 await m.react('✅')
 } catch {
