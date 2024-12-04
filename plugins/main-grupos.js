747d0ab3b3a39149541e0e554b62ae2492bbd59d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-03 20:37:19 -0400

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://i.ibb.co/3N4StyG/file.jpg`)).buffer()
+let img = await (await fetch(`https://pomf2.lain.la/f/usrof2b.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
