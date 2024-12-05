007e20e22d59a2a842453f21f3179ffeb01788d8
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 16:18:08 -0400

    Actualizar start-unreg.js

--- plugins/start-unreg.js
+++ plugins/start-unreg.js
@@ -8,7 +8,7 @@ let handler = async function (m, { conn, args, usedPrefix}) {
   m.reply(`🤍 Registro eliminado.`)
 }
 handler.help = ['unreg'] 
-handler.tags = ['rg']
+handler.tags = ['start']
 handler.command = ['unreg'] 
 handler.register = true
 
