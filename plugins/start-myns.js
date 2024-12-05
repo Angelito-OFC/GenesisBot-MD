00c826c95dfba1d6993b0adfb58aa08ac96dea96
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 16:17:44 -0400

    Actualizar start-myns.js

--- plugins/start-myns.js
+++ plugins/start-myns.js
@@ -5,7 +5,7 @@ let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)
 await conn.reply(m.chat, `${sn}`, m, rcanal)
 }
 handler.help = ['sn']
-handler.tags = ['rg']
+handler.tags = ['start']
 handler.command = ['nserie', 'sn', 'mysn'] 
 handler.register = true
 export default handler
