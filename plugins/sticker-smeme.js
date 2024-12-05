c96ed352755cfff71ceeac09923150e9f4d3c77d
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 19:27:22 -0400

    Actualizar sticker-smeme.js

--- plugins/sticker-smeme.js
+++ plugins/sticker-smeme.js
@@ -1,24 +1,21 @@
-import uploadImage from '../lib/uploadImage.js'
+import { uploadPomf } from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let [atas, bawah] = text.split`|`
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!mime) return m.reply(`🚩 Responde a una imagen e ingresa un texto junto al comando.`)
-    try {
-    if (!/image\/(jpe?g|png)/.test(mime)) return await m.react('✖️')
-    await m.react('🕓')
+    if (!mime) throw m.reply(`✧ Responde a una *Imagen* con el comando\n\n${usedPrefix + command} <${atas ? atas : 'Texto Arriba'}>|<${bawah ? bawah : 'Texto Bajo'}>`)
+    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
     let img = await q.download()
-    let url = await uploadImage(img)
+    let url = await uploadPomf(img)
     let meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '')}/${encodeURIComponent(bawah ? bawah : '')}.png?background=${url}`
-    let stiker = await sticker(false, meme, global.packname, global.author)
-    if (stiker) await conn.sendFile(m.chat, stiker, '', author, m, '', { asSticker: 1 })
-    await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['smeme *<texto>*']
-handler.tags = ['sticker']
+    let stiker = await sticker(false, meme, global.stickpack, global.wm)
+    if (stiker) await conn.sendFile(m.chat, stiker, '', m, '', { asSticker: 1 })
+}
+handler.help = ['smeme <txt>|<txt>']
+handler.tags = ['tools']
 handler.command = /^(smeme)$/i
-handler.register = true 
+
+handler.register = true
+
 export default handler
