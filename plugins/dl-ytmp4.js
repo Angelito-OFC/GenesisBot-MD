75bb824d12f20336204f08958e426b700cac46bd
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 15:37:18 -0400

    Actualizar dl-ytmp4.js

--- plugins/dl-ytmp4.js
+++ plugins/dl-ytmp4.js
@@ -37,7 +37,7 @@ await m.react('✅')
 await m.react('✖️')
 }}}
 handler.help = ['ytmp4 *<link yt>*']
-handler.tags = ['downloader']
+handler.tags = ['dl']
 handler.command = ['ytmp4', 'ytv', 'yt']
 //handler.limit = 1
 handler.register = true 
