cfc2927a0b65f916d7c365038c8d87e8959d83d4
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 15:27:49 -0400

    Actualizar dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,18 +1,47 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import { snapsave } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) return conn.reply(m.chat, '🚩 Ingresa el enlace del vídeo de Instagram junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://www.instagram.com/p/C60xXk3J-sb/?igsh=YzljYTk1ODg3Zg==`, m, rcanal)
-await m.react('🕓')
-try {
-let { dl_url } = await Starlights.igdl(args[0])
-await conn.sendFile(m.chat, dl_url, 'igdl.mp4', listo, m, null, rcanal)
-await m.react('✅')
-} catch {
-await m.react('✖️')
-}}
-handler.help = ['instagram *<link ig>*']
-handler.tags = ['downloader']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
-//handler.limit = 1
-handler.register = true 
+let handler = async (m, { conn, args }) => {
+    if (!args[0]) throw m.reply('✧ Ingresa el link de *Instagram*')
+    const sender = m.sender.split('@')[0];
+    const url = args[0];
+
+    m.reply(wait);
+
+    try {
+        const data = await snapsave(url);
+        
+        // Find the HD video
+        let video = data.results[0];
+
+        if (video) {
+            const videoBuffer = await fetch(video.url).then(res => res.buffer());
+            const caption = `✧ Para: @${sender}`;
+
+            await conn.sendMessage(
+                m.chat, {
+                    video: videoBuffer,
+                    mimetype: "video/mp4",
+                    fileName: `video.mp4`,
+                    caption: caption,
+                    mentions: [m.sender],
+                }, {
+                    quoted: m
+                }
+            );
+        } else {
+            throw m.reply('Error');
+        }
+    } catch (error) {
+        console.error('Handler Error:', error);
+        conn.reply(m.chat, `Error: ${error}`, m);
+    }
+}
+
+handler.help = ['ig'].map(v => v + ' <link>')
+handler.tags = ['dl']
+
+handler.command = /^(ig(dl)?)$/i
+handler.limit = true
+handler.register = true
+
 export default handler
