ff06c99bb94b7fa1b7e218d0dafbccf780af461e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 22:43:24 -0400

    Actualizar search-tiktok.js

--- plugins/search-tiktok.js
+++ plugins/search-tiktok.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Starlights from '@StarlightsTeam/Scraper'
 
 let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-  if (!text) return conn.reply(m.chat, `🚩 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Ai Hoshino Edit`, m, rcanal)
+  if (!text) return conn.reply(m.chat, `🤍 Ingresa el nombre video que deseas buscar en TikTok.\n\nEjemplo:\n> *${usedPrefix + command}* Ai Genesis Edit`, m, rcanal)
   
   await m.react('🕓')
   let img = await (await axios.get('https://i.ibb.co/kyTcqt9/file.jpg', { responseType: 'arraybuffer' })).data
