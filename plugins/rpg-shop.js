0c9487a405bd9212e8d0fe4beed83bae2c5698bd
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 22:18:04 -0400

    Actualizar rpg-shop.js

--- plugins/rpg-shop.js
+++ plugins/rpg-shop.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buycoins/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (global.db.data.users[m.sender].exp >= xppercorazones * count) {
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xppercorazones * count
     global.db.data.users[m.sender].corazones += count
     conn.reply(m.chat, `╭────═[ *R P G  -  S H O P* ]═─────⋆
