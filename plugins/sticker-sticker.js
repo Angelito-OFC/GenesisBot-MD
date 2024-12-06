15b287989736f0e2e24c59410a75ffcf5a6c6147
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 20:16:08 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -4,55 +4,55 @@ import { addExif } from '../lib/sticker.js'
 import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  try {
+  let stiker = false
+       let stick = args.join(" ").split("|");
+       let f = stick[0] !== "" ? stick[0] : packname;
+       let g = typeof stick[1] !== "undefined" ? stick[1] : author;
+  try { 	
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
-
-    if (/video/g.test(mime)) {
-      // Jalankan kode untuk video di sini
-      if ((q.msg || q).seconds > 10) return m.reply('✧ Máximo 10 segundos.')
+    if (/webp|image|video/g.test(mime)) {
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Máximo 10 segundos')
       let img = await q.download?.()
-      if (!img) throw m.reply(`✧ Responde a un Vídeo con el comando*${usedPrefix + command}*`)
-      let stiker = false
+      if (!img) throw `✳️ Responde a una imagen o video con*${usedPrefix + command}*`
+m.react('🐈‍⬛') 
+      let out
       try {
-        stiker = await sticker(img, false, global.stickpack, global.stickauth)
+        stiker = await sticker(img, false, f, g)
       } catch (e) {
         console.error(e)
       } finally {
         if (!stiker) {
-          let out = await uploadFile(img)
-          stiker = await sticker(false, out, global.stickpack, global.stickauth)
+          if (/webp/g.test(mime)) out = await webp2png(img)
+          else if (/image/g.test(mime)) out = await uploadImage(img)
+          else if (/video/g.test(mime)) out = await uploadFile(img)
+          if (typeof out !== 'string') out = await uploadImage(img)
+          stiker = await sticker(false, out, f, g)
         }
       }
-    await conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, rpl)
-    } else if (/image/g.test(mime)) {
-      // Jalankan kode untuk gambar di sini
-      let [packname, ...author] = args.join` `.split`|`
-      author = (author || []).join`|`
-      let img = await q.download?.()
-      let stiker = false
-      try {
-        let pack = global.stickpack
-        let author = global.stickauth
-        stiker = await addExif(img, pack, author)
-      } catch (e) {
-        console.error(e)
-      } finally {
-        if (!stiker) {
-          stiker = await createSticker(img, false, packname, author)
-        }
-      }
-      m.reply(stiker)
-    } else {
-      m.reply('     `✧ Responde a una Imagen o Video.`')
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL invalido')
     }
   } catch (e) {
     console.error(e)
-    m.reply('Error')
+    if (!stiker) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null, rpl)
+    else throw '➤ `𝗔𝗩𝗜𝗦𝗢` 🐈‍⬛\n\n*RESPONDE UNA IMG/VIDEO*';
   }
 }
-
 handler.help = ['sticker']
+handler.tags = ['sticker']
+handler.command = ['s', 'sticker'] 
+
+export default handler
+
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
+}
+
+/* handler.help = ['sticker']
 handler.tags = ['sticker']
 
 handler.command = /^s(tic?ker)?(gif)?$/i
@@ -68,4 +68,4 @@ async function createSticker(img, url, packName, authorName, quality) {
     quality
   }
   return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
-}
+} */
