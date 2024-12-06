e9f3357531992b93ce72e12703c5a915c25d356c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 20:19:54 -0400

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -4,6 +4,82 @@ import { addExif } from '../lib/sticker.js'
 import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+
+    if (/video/g.test(mime)) {
+      // Jalankan kode untuk video di sini
+      if ((q.msg || q).seconds > 10) return m.reply('✧ Máximo 10 segundos.')
+      let img = await q.download?.()
+      if (!img) throw m.reply(`✧ Responde a un Vídeo con el comando*${usedPrefix + command}*`)
+      let stiker = false
+      try {
+        stiker = await sticker(img, false, global.stickpack, global.stickauth)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          let out = await uploadFile(img)
+          stiker = await sticker(false, out, global.stickpack, global.stickauth)
+        }
+      }
+      conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, null)
+    } else if (/image/g.test(mime)) {
+      // Jalankan kode untuk gambar di sini
+      let [packname, ...author] = args.join` `.split`|`
+      author = (author || []).join`|`
+      let img = await q.download?.()
+      let stiker = false
+      try {
+        let pack = global.stickpack
+        let author = global.stickauth
+        stiker = await addExif(img, pack, author)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        if (!stiker) {
+          stiker = await createSticker(img, false, packname, author)
+        }
+      }
+      m.reply(stiker)
+    } else {
+      m.reply('     `✧ Responde a una Imagen o Video.`')
+    }
+  } catch (e) {
+    console.error(e)
+    m.reply('Error')
+  }
+}
+
+handler.help = ['sticker']
+handler.tags = ['sticker']
+
+handler.command = /^s(tic?ker)?(gif)?$/i
+handler.register = true
+
+export default handler
+
+async function createSticker(img, url, packName, authorName, quality) {
+  let stickerMetadata = {
+    type: 'full',
+    pack: stickpack,
+    author: stickauth,
+    quality
+  }
+  return (new Sticker(img ? img : url, stickerMetadata)).toBuffer()
+}
+
+
+
+
+
+/* import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import { addExif } from '../lib/sticker.js'
+import { Sticker } from 'wa-sticker-formatter'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
        let stick = args.join(" ").split("|");
        let f = stick[0] !== "" ? stick[0] : packname;
@@ -52,7 +128,7 @@ const isUrl = (text) => {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
 
-/* handler.help = ['sticker']
+handler.help = ['sticker']
 handler.tags = ['sticker']
 
 handler.command = /^s(tic?ker)?(gif)?$/i
