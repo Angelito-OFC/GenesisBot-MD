ce05321c09db85c50c81da7cdcdb6d7f50435297
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 22:12:52 -0400

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,11 +1,11 @@
-
+ 
 
 let handler = async (m, {conn, usedPrefix}) => {
    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
    if (who == conn.user.jid) return m.react('✖️')
    if (!(who in global.db.data.users)) return m.reply(`*El usuario no se encuentra en mi base de datos*`)
    let user = global.db.data.users[who]
-   await m.reply(`${who == m.sender ? `Tienes *${user.bank} ⭐ Estrellas* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} ⭐ Estrellas* en el Banco`}`, null, { mentions: [who] })
+   await m.reply(`${who == m.sender ? `Tienes *${user.bank} 🤍 Corazones* en el Banco` : `El usuario @${who.split('@')[0]} tiene *${user.bank} 🤍 Corazones* en el Banco`}`, null, { mentions: [who] })
 }
 
 handler.help = ['bank']
