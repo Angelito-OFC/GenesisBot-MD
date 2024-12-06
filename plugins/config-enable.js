10fa2b17c23c617e26e920c15614f17d6311b56f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 23:09:52 -0400

    Actualizar config-enable.js

--- plugins/config-enable.js
+++ plugins/config-enable.js
@@ -1,5 +1,28 @@
 
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
+
+const miniopcion = `🤍 *OPCIONES PARA GRUPOS*
+
+welcome
+antiTiktok
+antiYoutube
+antiTelegram
+antiFacebook
+antiInstagram
+antiTwitter
+antiDiscord
+antiver
+nsfw
+antiarabes
+antilink
+autoread
+document
+
+*ejemplo*
+
+.on welcome
+.off welcome`
+
   let isEnable = /true|enable|(turn)?on|1/i.test(command)
   let chat = global.db.data.chats[m.chat]
   let user = global.db.data.users[m.sender]
@@ -159,43 +182,7 @@ break
        chat.onlyLatinos = isEnable  
        break
     default:
-      if (!/[01]/.test(command)) return m.reply(`
-*🚩 Ingresa una opción para habilitar o deshabilitar*
-
-*≡ Lista de opciones*
-*Tipo :* welcome
-*Descripción :* Des/Activa la *Bienvenida* y *Despedida* para Grupos
-
-antiTiktok
-antiYoutube
-antiTelegram
-antiFacebook
-antiInstagram
-antiTwitter
-antiDiscord
-antiver
-
-*Tipo :* nsfw 
-*Descripción :* Des/Activa los comandos *NSFW* para Grupos
-
-*Tipo :* antiarabes 
-*Descripción :* Des/Activa el *AntiArabes* para Grupos
-
-*Tipo :* antilink 
-*Descripción :* Des/Activa el *AntiLink* para Grupos
-
-*Tipo :* antiver 
-*Descripción :* Des/Activa el *antiver* para Grupos
-
-*Tipo :* autoread 
-*Descripción :* Des/Activa el *AutoRead* para el Bot
-
-*Tipo :* document 
-*Descripción :* Des/Activa la *Descarga En Documentos* para el Usuario
-
-*• Ejemplo:*
-*- ${usedPrefix + command}* welcome
-`.trim())
+if (!/[01]/.test(command)) return await conn.reply(m.chat, miniopcion, m, rcanal);
       throw false
   }
 conn.sendMessage(m.chat, {text: `\`❱❱ 𝗚𝗘𝗡𝗘𝗦𝗜𝗦 • 𝗠𝗗 ❰❰\`\n\n*» 𝗢𝗣𝗖𝗜𝗢𝗡 |* _${type.toUpperCase()}_\n*» 𝗘𝗦𝗧𝗔𝗗𝗢 |* ${isEnable ? 'ON' : 'OFF'}\n*» 𝗣𝗔𝗥𝗔 |* ${isAll ? 'ESTE BOT' : isUser ? '' : 'ESTE CHAT'}\n> ${stickpack}`}, {quoted: estilo});
