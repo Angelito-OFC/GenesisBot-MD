8147322fbe22aabee99e855d6f670c0ad7c8fd7f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 22:06:35 -0400

    Actualizar rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -12,7 +12,7 @@ let text = `
 ╭───═[ *Top ${len} Corazones 🤍* ]═────⋆
 │╭───────────────···
 ✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLim.slice(0, len).map(({ jid, limit }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🤍*`).join`\n✩│ `}
+✩│ ${sortedLim.slice(0, len).map(({ jid, corazones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🤍*`).join`\n✩│ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
