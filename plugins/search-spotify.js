ea0f76fcc615c2c75f5609975443495c00a4e54c
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 23:40:22 -0400

    Actualizar search-spotify.js

--- plugins/search-spotify.js
+++ plugins/search-spotify.js
@@ -43,7 +43,7 @@ buttons: [ */
                 buttons: [
 {
 "name": "cta_copy",
-"buttonParamsJson": "{\"display_text\":\"🎧 ¡Descargar Audio! 🎧\",\"id\":\"123456789\",\"copy_code\":\".spotify" + track.url + "\"}"
+"buttonParamsJson": "{\"display_text\":\"🎧 ¡Descargar Audio! 🎧\",\"id\":\"123456789\",\"copy_code\":\".spotify " + track.url + "\"}"
 },
 ]
 })
