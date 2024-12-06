8e394bbfa3f225a60180c680421506019215eb6f
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-05 22:07:52 -0400

    Actualizar rpg-lb.js

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -9,12 +9,10 @@ let usersLim = sortedLim.map(enumGetKey)
 let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 let text = `
-╭───═[ *Top ${len} Corazones 🤍* ]═────⋆
-│╭───────────────···
-✩│ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-✩│ ${sortedLim.slice(0, len).map(({ jid, corazones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🤍*`).join`\n✩│ `}
-│╰────────────────···
-╰───────────═┅═──────────
+*\`Top ${len} Corazones 🤍\`*
+✩ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
+✩ ${sortedLim.slice(0, len).map(({ jid, corazones }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${corazones} 🤍*`).join`\n✩│ `}
+
 
 ╭───═[ *TOP ${len} XP 💫* ]═────⋆
 │╭───────────────···
