4dd724473d07a1e30cfbc5da32cbfa9415d47102
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-07 12:09:27 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -62,9 +62,12 @@ global.catalogo = fs.readFileSync('./storage/img/catalogo.png')
 global.miniurl = fs.readFileSync('./storage/img/miniurl.jpg')
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-global.pagina = 'atom.bio/masha_ofc'
-global.group = ''
-global.canal = 'https://whatsapp.com/channel/0029Vak9Hmd1iUxdfDUdCK1w'
+global.group = 'https://chat.whatsapp.com/GqKwwoV2JJaJDP2SL7SddX'
+global.group2 = 'https://chat.whatsapp.com/Fn5Ipyxu6mE6qEQlwWZTwU'
+global.canal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'
+global.github = 'https://github.com/Angelito-OFC/GenesisBot-MD' 
+global.instagram = 'https://www.instagram.com/usxr_angelito0' 
+global.whatsApp = 'https://wa.me/59168683798'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
