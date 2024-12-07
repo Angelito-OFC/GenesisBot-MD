595f882a95a94837c1ac87b2b8fd0519a990ddf2
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-07 17:00:46 -0400

    Actualizar test2.js

--- plugins/test2.js
+++ plugins/test2.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     );
   }
 
-  await m.react('⏳'); // Indica que el proceso está en curso.
+  await m.react('⏳'); 
 
   try {
     const result = await fluximg.create(text);
@@ -22,13 +22,13 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         },
         { quoted: m }
       );
-      await m.react('✅'); // Reacciona al completar.
+      await m.react('✅'); 
     } else {
       throw new Error("Error al crear la imagen. Por favor, inténtalo nuevamente.");
     }
   } catch (error) {
     console.error(error);
-    await m.react('❌'); // Indica que hubo un error.
+    await m.react('❌'); 
     conn.reply(m.chat, "Ocurrió un error al generar la imagen. Inténtalo de nuevo.", m);
   }
 };
@@ -40,7 +40,7 @@ handler.command = ["flux"];
 export default handler;
 
 const fluximg = {
-  defaultRatio: "2:3", // Relación de aspecto predeterminada.
+  defaultRatio: "2:3", 
 
   create: async (query) => {
     const apiUrl = `https://1yjs1yldj7.execute-api.us-east-1.amazonaws.com/default/ai_image`;
@@ -50,7 +50,7 @@ const fluximg = {
         authority: "1yjs1yldj7.execute-api.us-east-1.amazonaws.com",
         "user-agent": "Postify/1.0.0",
       },
-      timeout: 5000, // Establece un tiempo de espera de 5 segundos.
+      timeout: 5000, 
     };
 
     try {
