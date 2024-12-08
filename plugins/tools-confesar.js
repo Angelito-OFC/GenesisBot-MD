ece000895972a0b3128914566ee4ac25241c61df
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-07 22:31:18 -0400

    Actualizar tools-confesar.js

--- plugins/tools-confesar.js
+++ plugins/tools-confesar.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
                 mentionedJid: [data.jid],
                 externalAdReply: {
                     title: 'C O N F E S A R',
-                    body: 'hola',
+                    body: '¡Para responder usa! . respuesta <id> <mensaje>',
                     mediaType: 1,
                     previewType: 0,
                     renderLargerThumbnail: true,
