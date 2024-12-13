2966bbcf0c47872635f6428d953fdaa9ff75d930
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-12-13 12:49:52 -0400

    Actualizar serbot-delserbot.js

--- plugins/serbot-delserbot.js
+++ plugins/serbot-delserbot.js
@@ -1,19 +1,17 @@
 import { readdirSync, statSync, unlinkSync, existsSync, readFileSync, watch, rmSync, promises as fs} from "fs"
 import path, { join } from 'path'
-import { areJidsSameUser, jidNormalizedUser } from '@whiskeysockets/baileys';
 
 let handler  = async (m, { conn: parentw, usedPrefix, command}, args) => {
 
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let uniqid = `${who.split`@`[0]}`
 let userS = `${conn.getName(who)}`
-let numerxd = jidNormalizedUser(who);
 
 try {
-await fs.rmdir("./sessions/" + numerxd, { recursive: true, force: true })
-await parentw.sendMessage(m.chat, { text: '🤍 Sub-Bot eliminado.' }, { quoted: m })
+await fs.rmdir("./serbot/" + uniqid, { recursive: true, force: true })
+await parentw.sendMessage(m.chat, { text: '🚩 Sub-Bot eliminado.' }, { quoted: m })
 } catch(err) {
-if (err.code === 'ENOENT' && err.path === `./sessions/${uniqid}`) {
+if (err.code === 'ENOENT' && err.path === `./serbot/${uniqid}`) {
 await parentw.sendMessage(m.chat, { text: "No cuentas con ninguna sesión de Sub-Bot." }, { quoted: m })
 } else {
 await m.react('✖️')
